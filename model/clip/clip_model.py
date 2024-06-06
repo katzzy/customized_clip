38eb918a68691fec999e79dc07f8d2ef39201ed1
Korbin Ding <jianbind@foxmail.com>
2024-06-06 16:12:05 +0800

    refactor: :recycle: remove unnecessary code

--- model/clip/clip_model.py
+++ model/clip/clip_model.py
@@ -342,26 +342,6 @@ class TextTransformer(nn.Module):
 
         self.text_projection = nn.Parameter(torch.empty(transformer_width, embed_dim))
 
-        self.initialize_parameters()
-
-    def initialize_parameters(self):
-        nn.init.normal_(self.token_embedding.weight, std=0.02)
-        nn.init.normal_(self.positional_embedding, std=0.01)
-
-        proj_std = (self.transformer.width**-0.5) * (
-            (2 * self.transformer.layers) ** -0.5
-        )
-        attn_std = self.transformer.width**-0.5
-        fc_std = (2 * self.transformer.width) ** -0.5
-        for block in self.transformer.resblocks:
-            nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
-            nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
-            nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
-            nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
-
-        if self.text_projection is not None:
-            nn.init.normal_(self.text_projection, std=self.transformer.width**-0.5)
-
     def build_attention_mask(self):
         # lazily create causal attention mask,
         # with full attention between the vision tokens
@@ -409,8 +389,6 @@ class CLIP(nn.Module):
         transformer_layers: int,
     ):
         super().__init__()
-
-        self.context_length = context_length
 
         if isinstance(vision_layers, (tuple, list)):
             vision_heads = vision_width * 32 // 64
